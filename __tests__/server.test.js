d546761647b5fe2bb824c80706a84d2e84e984c1
KynanPR <kynanpr@gmail.con>
2025-04-28 10:39:34 +0100

    Set up test file

---
+++ __tests__/server.test.js
@@ -0,0 +1,21 @@
+const endpointsJson = require("../endpoints.json");
+const server = require("../api/server");
+const testData = require("../db/data/test-data/index");
+const seed = require("../db/seeds/seed");
+const db = require("../db/connection");
+const testReq = require("supertest");
+
+beforeEach(() => seed(testData));
+
+afterAll(() => db.end());
+
+describe("GET /api", () => {
+  test("200: Responds with an object detailing the documentation for each endpoint", () => {
+    return testReq(server)
+      .get("/api")
+      .expect(200)
+      .then(({ body: { endpoints } }) => {
+        expect(endpoints).toEqual(endpointsJson);
+      });
+  });
+});
