a8dea4c5f4a853c7a5159fbf43a81910c53c7b7b
KynanPR <kynanpr@gmail.con>
2025-04-28 11:47:49 +0100

    Added general error handling

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -8,6 +8,16 @@ const testReq = require("supertest");
 beforeEach(() => seed(testData));
 
 afterAll(() => db.end());
+describe("Bad Endpoint", () => {
+  test("404: Responds with not found message", async () => {
+    const { body } = await testReq(server)
+      .get("/api/thisDoesntExist")
+      .expect(404);
+    expect(body.message).toBe(
+      "Endpoint/Method doesn't exist. Try GET /api to see info on all available endpoints"
+    );
+  });
+});
 
 describe("GET /api", () => {
   test("200: Responds with an object detailing the documentation for each endpoint", () => {
--- api/server.js
+++ api/server.js
@@ -6,4 +6,23 @@ server.use(express.json());
 
 server.get("/api", getApiEndpoints);
 
+server.use((err, req, res, next) => {
+  if (err.status && err.message) {
+    res.status(err.status).json({ message: err.message });
+  } else next(err);
+});
+
+server.use((err, req, res, next) => {
+  if (err.code === "22P02") {
+    res.status(400).json({ message: "Bad Request" });
+  } else next(err);
+});
+
+server.all("/*splat", (req, res, next) => {
+  res.status(404).json({
+    message:
+      "Endpoint/Method doesn't exist. Try GET /api to see info on all available endpoints",
+  });
+});
+
 module.exports = server;
