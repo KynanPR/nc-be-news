cdc67fcbc129966315be53d22b6c817f9aabddba
KynanPR <kynanpr@gmail.con>
2025-04-29 15:18:11 +0100

    Removed message property from all non error responses

--- api/controllers/topics.controller.js
+++ api/controllers/topics.controller.js
@@ -4,7 +4,6 @@ exports.getAllTopics = async (req, res, next) => {
   try {
     const topics = await selectAllTopics();
     const resBody = {
-      message: "All Topics currently in database",
       topics,
     };
     res.json(resBody);
